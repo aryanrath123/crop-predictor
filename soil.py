2d30a6ea822de4133290ccf2371f5ca1535db65e
aryanrath123 <your-email@example.com>
2024-12-11 12:12:24 +0530

    Update requirements.txt and clean up for deployment

--- soil.py
+++ soil.py
@@ -36,6 +36,6 @@ def predict():
         return jsonify({'recommended_crop': crop_name[0]})
     except Exception as e:
         return jsonify({'error': str(e)}), 400
-
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
